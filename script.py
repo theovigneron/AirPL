67433a8437a78e9500350e4db3b4a96be48f760c
Mikhail Kuchukhidze <mikhail.kuchukhidze@isento-ecommerce.de>
2023-05-21 10:42:36 +0200

    Ajout calcul emission/heure

--- script.py
+++ script.py
@@ -1,20 +1,3 @@
-import requests
-
-url = "https://geo.api.gouv.fr/communes"
-params = {
-    "codeRegion": "52",
-    "fields": "nom,code,population",
-    "format": "json",
-    "geometry": "centre"
-}
-
-response = requests.get(url, params=params)
-communes = response.json()
-
-# print(communes)
-
-
-
 import json
 import pandas as pd
 
@@ -34,18 +17,10 @@ periode_results = data['results']
 # Créer un DataFrame à partir des résultats filtrés
 df = pd.DataFrame(periode_results)
 
-#TODO garder la valeur de PM10 maximale pour chaque commune
-print(df.columns)
-
-columns_to_keep = ['nom_commune', 'valeur', "date_heure_tu"]  # Add the desired column names here
+columns_to_keep = ['nom_commune', 'valeur', "date_heure_tu"]  
 
 df_max_pm10_idx = df.groupby('nom_commune')['valeur'].idxmax()
 df_max_pm10 = df.loc[df_max_pm10_idx.dropna(), columns_to_keep]
-
-# Calculer les statistiques de la période
-total_expositions = len(df)
-total_depassements_journaliers = df[df['valeur'].gt(valeur_limite_journaliere)].shape[0]
-total_depassements_annuels = df[df['valeur'].gt(valeur_limite_annuelle)].shape[0]
 
 # Générer le rapport
 rapport = []
@@ -63,16 +38,42 @@ rapport.append(" - Seuil d'alerte sur 24 heures : " + str(seuil_alerte) + " μg/
 # Convertir la liste en chaîne de caractères
 rapport_str = "\n".join(rapport)
 
-# Afficher le rapport
+# Afficher l'introduction
 print(rapport_str)
 
-
-
-# Perform a left join to include all communes
-# df_selected_columns = df[['nom_commune']].merge(df_max_pm10, on='nom_commune', how='left')
-
-# df_selected_columns = df_selected_columns.drop_duplicates(subset='nom_commune')
-
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    print("\nRésultats détaillés (PM10 maximal par commune) :")
+    print("\nPM10 maximal enregistré par commune :")
     print(df_max_pm10)
+    
+
+
+# Load data from JSON file
+with open('PM10_Horaire_OctobreMars.json', 'r') as file:
+    data = json.load(file)
+
+periode_results = data['results']
+
+df = pd.DataFrame(periode_results)
+
+# Convert 'date_heure_tu' column to datetime type
+df['date_heure_tu'] = pd.to_datetime(df['date_heure_tu'])
+
+df.dropna(subset=['nom_commune'], inplace=True)
+
+df['heure'] = df['date_heure_tu'].dt.hour
+
+# Group by commune et calculer la moyenne des émissions de PM10
+df_avg_pm10 = df.groupby(['nom_commune', 'heure'])['valeur'].mean().reset_index()
+
+# trouver l'heure avec les émissions moyennes les plus élevées par commune
+max_avg_pm10 = df_avg_pm10.groupby('nom_commune')['valeur'].max()
+
+# On garde que les lignes avec les émissions moyennes les plus élevées par commune
+df_max_avg_pm10 = df_avg_pm10[df_avg_pm10.groupby('nom_commune')['valeur'].transform(lambda x: x == x.max())]
+
+print("Heure avec les émissions moyennes les plus élevées par commune:")
+for index, row in df_max_avg_pm10.iterrows():
+    commune = row['nom_commune']
+    heure = row['heure']
+    moyenne_emissions = row['valeur']
+    print(f"Commune: {commune}, Heure: {heure}, Emission de PM10 en moyenne: {moyenne_emissions}")
